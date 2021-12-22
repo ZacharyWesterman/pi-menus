d38c0b2694dee297d6484fc1d6dbe71d796376d7
Zachary Westerman <westerman.zachary@gmail.com>
2021-12-22 00:53:01 -0600

    rename test.py to main.py since it will be the entry point

--- main.py
+++ main.py
@@ -1,168 +1,61 @@
-﻿#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-import RPi.GPIO as GPIO
-import time, traceback, subprocess
-from PIL import Image, ImageDraw, ImageFont
+import display
+import json
+import variables
+import subprocess
 
-import SH1106.SH1106 as SH1106
-import SH1106.config as config
+if __name__ == '__main__':
+	try:
+		display.start()
 
-import textMenu
-import keyboard
-import pins
+		with open('menu.json', 'r') as fp:
+			menu_display = json.load(fp)
 
-MENU_EXIT = False
+		menus = ['main']
+		# display.put('input: ', 0, 0)
+		# result = display.get()
 
-try:
-	pins.init()
-	disp = SH1106.SH1106()
+		while True:
+			this_menu = menu_display[menus[-1]]
+			try:
+				display.message('Loading...')
+				result = display.menu(this_menu)
+				this_option = display.build_options(this_menu)[result]
 
-	print("1.3inch OLED")
-	# Initialize library.
-	disp.Init()
-	# Clear display.
-	disp.clear()
+				if 'input' in this_option:
+					value = display.get('password' in this_option['input'] and this_option['input']['password'])
+					variables.set('line', this_option['text'])
+					variables.set('item', this_option['text'].split())
+					value = variables.set(this_option['input']['var'], value)
 
-	"""
-	menu = textMenu.textMenu(disp)
-	menu.options((
-		'Keyboard',
-		'Exit',
-	));
-	menu.draw(True)
+				if 'action' in this_option:
+					try:
+						display.message('Processing...')
+						subprocess.check_output(this_option['action'], shell=True)
+					except:
+						pass
 
-	# Menu selection
-	holding = False
-	while True:
-		keyPin = -1
+				if 'return' in this_option and this_option['return']:
+					menus = menus[:-1]
+					if not len(menus): break
+				elif 'goto' in this_option:
+					menus += [this_option['goto']]
+			except display.CancelInput:
+				menus = menus[:-1]
+				if not len(menus): break
 
-		if not GPIO.input(KEY_UP_PIN):
-			menu.up()
-			keyPin = KEY_UP_PIN
-		if not GPIO.input(KEY_DOWN_PIN):
-			menu.down()
-			keyPin = KEY_DOWN_PIN
-		if not (GPIO.input(KEY_RIGHT_PIN) and GPIO.input(KEY_PRESS_PIN)):
-			if menu.terminal():
-				if menu.path() == 'Exit':
-					disp.clear()
-					exit()
-				if menu.path() == 'Keyboard':
-					menu.draw(False)
-					break
-			menu.select()
-			keyPin = KEY_RIGHT_PIN
-		if not GPIO.input(KEY_LEFT_PIN):
-			menu.back()
-			keyPin = KEY_LEFT_PIN
+			# print(menus)
 
-		# Delay menu selections on key press
-		if keyPin > -1:
-			menuDelay = True
-			for i in range(0,10):
-				time.sleep(0.01)
-				if GPIO.input(keyPin):
-					menuDelay = False
-					holding = False
-					break
-			if menuDelay:
-				if holding:
-					time.sleep(0.02)
-				else:
-					time.sleep(0.5)
+		display.stop()
+		exit()
 
-				holding = False if GPIO.input(keyPin) else True
-	"""
+	except KeyboardInterrupt:
+		display.stop()
+		print("User signalled interrupt. Exiting.")
+		exit()
 
-	# Show keyboard
-	keys = keyboard.keyboard(disp)
-	keys.hidden = True
-	keys.draw(True)
-
-
-
-	def menu_back(_=None):
-		global MENU_EXIT
-		if keys.text == '':
-			MENU_EXIT = True
-		else:
-			keys.backspace()
-
-	def menu_fwd(_=None):
-		global MENU_EXIT
-		print(">>"+keys.text)
-		MENU_EXIT = True
-
-	GPIO.add_event_detect(pins.KEY_LEFT, GPIO.FALLING, callback=keys.left, bouncetime=200)
-	GPIO.add_event_detect(pins.KEY_RIGHT, GPIO.FALLING, callback=keys.right, bouncetime=200)
-	GPIO.add_event_detect(pins.KEY_UP, GPIO.FALLING, callback=keys.up, bouncetime=200)
-	GPIO.add_event_detect(pins.KEY_DOWN, GPIO.FALLING, callback=keys.down, bouncetime=200)
-	GPIO.add_event_detect(pins.KEY_PRESS, GPIO.FALLING, callback=keys.select, bouncetime=250)
-	GPIO.add_event_detect(pins.KEY1, GPIO.FALLING, callback=keys.toggleShift, bouncetime=250)
-	GPIO.add_event_detect(pins.KEY2, GPIO.FALLING, callback=menu_fwd, bouncetime=250)
-	GPIO.add_event_detect(pins.KEY3, GPIO.FALLING, callback=menu_back, bouncetime=250)
-
-	while True:
-		if MENU_EXIT: break
-		time.sleep(0.1)
-
-	"""
-	holding = False
-	while True:
-		keyPin = -1
-
-		if not GPIO.input(pins.KEY1):
-			keys.toggleShift()
-			keyPin = pins.KEY1
-		if not GPIO.input(pins.KEY2):
-			print(keys.text)
-			break
-		if not GPIO.input(pins.KEY3):
-			if keys.text == '': break
-			keys.backspace()
-			keyPin = pins.KEY3
-		if not GPIO.input(pins.KEY_RIGHT):
-			keys.right()
-			keyPin = pins.KEY_RIGHT
-		if not GPIO.input(pins.KEY_LEFT):
-			keys.left()
-			keyPin = pins.KEY_LEFT
-		if not GPIO.input(pins.KEY_UP):
-			keys.up()
-			keyPin = pins.KEY_UP
-		if not GPIO.input(pins.KEY_DOWN):
-			keys.down()
-			keyPin = pins.KEY_DOWN
-		if not GPIO.input(pins.KEY_PRESS):
-			keys.select()
-			keyPin = pins.KEY_PRESS
-
-		# Delay menu selections on key press
-		if keyPin > -1:
-			menuDelay = True
-			for i in range(0,10):
-				time.sleep(0.01)
-				if GPIO.input(keyPin):
-					menuDelay = False
-					holding = False
-					break
-			if menuDelay:
-				if holding:
-					time.sleep(0.02)
-				else:
-					time.sleep(0.5)
-
-				holding = False if GPIO.input(keyPin) else True
-
-	"""
-
-	disp.clear()
-
-except IOError as e:
-	print(e)
-
-except KeyboardInterrupt:
-	print("ctrl + c:")
-	disp.clear()
-	exit()
+	except Exception as e:
+		display.stop()
+		raise e
