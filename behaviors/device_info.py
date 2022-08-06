1ed3ca8cdae6af6be02b5c6cf859777e4b3c9a4f
Zachary Westerman <westerman.zachary@gmail.com>
2022-08-06 17:10:01 -0500

    All behaviors should be async, just in case some of them take a long time to process

--- behaviors/device_info.py
+++ behaviors/device_info.py
@@ -3,9 +3,9 @@ from behaviors import register
 import socket
 
 @register('get_ip_address')
-def get_ip_address(variables: object) -> str:
+async def get_ip_address(variables: object) -> str:
 	return socket.gethostbyname(socket.gethostname())
 
 @register('get_hostname')
-def get_hostname(variables: object) -> str:
+async def get_hostname(variables: object) -> str:
 	return socket.gethostname()
