115348bd7c592a9fa49729119136567e6bf89a61
Zachary Westerman <westerman.zachary@gmail.com>
2022-08-11 21:52:02 -0500

    when updating, make sure python deps are installed

--- behaviors/version_control.py
+++ behaviors/version_control.py
@@ -18,15 +18,30 @@ async def fetch_update(variables: object, display: object, **args) -> None:
 			print(stderr)
 			raise Exception(f'Failed to fetch update:\n{stderr}')
 
+		return stdout.decode().rstrip('\n')
+
 	def get_version():
 		with open('config/version.txt', 'r') as fp:
 			return fp.read().strip()
+
+	#Check if update is available
+	await run_cmd('git fetch')
+	updated_files = await run_cmd('git log --name-status origin/main..')
+
+	#If no updates available, exit
+	if updated_files == '':
+		await display.message('Already up-to-date.')
+		return
+
+	await display.message(title='Update Available', text='Installing Update...')
 
 	old_version = get_version()
 	await run_cmd('git pull --ff-only --quiet')
 	await run_cmd('chown pi:pi .git * -R')
 	new_version = get_version()
 
-	if old_version != new_version:
-		await display.message(title='Version Updated', subtitle=f'{old_version} to {new_version}', text='Please exit main menu \nto finish update.')
-		await display.user_acknowledge()
+	#ALWAYS make sure python dependencies are installed.
+	await run_cmd('venv/bin/python -m pip install -r requirements.txt --no-warn-script-location')
+
+	await display.message(title='Version Updated', subtitle=f'{old_version} to {new_version}', text='Please exit main menu \nto finish update.')
+	await display.user_acknowledge()
