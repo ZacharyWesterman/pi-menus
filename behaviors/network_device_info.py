aa220ce0d728f51dc38a4f76bef97e6da1152c1f
Zachary Westerman <westerman.zachary@gmail.com>
2022-09-30 22:26:14 -0500

    read correct vendor id

--- behaviors/network_device_info.py
+++ behaviors/network_device_info.py
@@ -15,7 +15,7 @@ async def get_device_info(variables: object, display: object, **args) -> dict:
 		msg += [f'ID  {name}']
 
 	try:
-		with open(f'config/mac_vendors/{mac[0:2]}/{mac}', 'r') as fp:
+		with open(f'config/mac_vendors/{mac[0:2]}/{mac[0:6]}', 'r') as fp:
 			msg += [fp.read()]
 	except FileNotFoundError:
 		msg += [f'MAC {mac}']
