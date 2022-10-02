d76053d2e947012875c99a500908db729ad76039
Zachary Westerman <westerman.zachary@gmail.com>
2022-10-01 19:12:45 -0500

    add option to reboot

--- behaviors/shutdown.py
+++ behaviors/shutdown.py
@@ -14,3 +14,16 @@ async def shutdown_action(display: object, **args) -> None:
 	stdout, stderr = await process.communicate()
 
 	await asyncio.sleep(20) #Don't display anything else
+
+@register('reboot')
+async def reboot_action(display: object, **args) -> None:
+	await display.message(title='Restarting...')
+	process = await asyncio.create_subprocess_shell(
+		'shutdown -r now',
+		shell = True,
+		stdout = asyncio.subprocess.PIPE,
+		stderr = asyncio.subprocess.PIPE
+	)
+	stdout, stderr = await process.communicate()
+
+	await asyncio.sleep(20) #Don't display anything else
